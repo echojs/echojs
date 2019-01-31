948648b23bcafe3fe12537a7f574a207ee891aa9
Frederic Cambus <fred@statdns.com>
2019-01-31 12:51:45 +0100

    Add unicorn configuration file.

---
+++ config/unicorn.rb
@@ -0,0 +1,22 @@
+# set path to app that will be used to configure unicorn, 
+# note the trailing slash in this example
+@dir = "/home/echojs/app/"
+
+worker_processes 2
+working_directory @dir
+
+preload_app true
+
+timeout 30
+
+# Specify path to socket unicorn listens to,
+# we will use this in our nginx.conf later
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
+
