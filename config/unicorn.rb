6c6c9362413dfa7340be53830be925e90d230411
Frederic Cambus <fred@statdns.com>
2019-05-08 19:12:02 +0200

    Fix path in unicorn configuration.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # set path to app that will be used to configure unicorn, 
 # note the trailing slash in this example
-@dir = "/home/echojs/app/"
+@dir = "/home/echojs/echojs/"
 
 worker_processes 2
 working_directory @dir
