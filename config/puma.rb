f6ce0d70445876cdeafb7505abbcead03a23e543
Fabien Allanic <fabien.allanic@gmail.com>
2026-06-22 19:13:03 -0600

    Create puma.rb

---
+++ config/puma.rb
@@ -0,0 +1,11 @@
+dir = "/home/echojs/echojs/"
+
+workers 2
+threads 1, 1
+
+preload_app true
+
+bind "unix://#{dir}tmp/sockets/puma.sock"
+
+pidfile "#{dir}tmp/pids/puma.pid"
+stdout_redirect "#{dir}log/puma.stdout.log", "#{dir}log/puma.stderr.log", true
